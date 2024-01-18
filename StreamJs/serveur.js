a49892b24644d30b37891ec1a0b804a8c535447a
Amirarbane <144345021+Amirarbane@users.noreply.github.com>
2024-01-18 16:05:19 +0100

    Add files via upload

--- StreamJs/serveur.js
+++ StreamJs/serveur.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path'); 
-const range = require('range-parser');
 const express = require('express')
 const app = express();
 
@@ -10,53 +9,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'Arbane')));
 // Toutes les autres requêtes redirigées vers le fichier HTML
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, 'video.mp4'));
+res.sendFile(path.join(__dirname, 'paysage.mp4'));
 });
 
 app.listen(3004, () => {
 console.log('Serveur démarré sur http://localhost:3004');
-});
-
-
-
-
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/video') {
-        const filePath = path.join(__dirname, 'video.mp4');
-        const stat = fs.statSync(filePath);
-        const fileSize = stat.size;
-
-        const rangeHeader = req.headers.range;
-        const ranges = range(fileSize, rangeHeader, { combine: true });
-
-        if (!ranges || ranges.type !== 'bytes' || ranges.length > 1) {
-            res.writeHead(200, {
-                'Content-Type': 'video/mp4',
-                'Content-Length': fileSize
-            });
-            fs.createReadStream(filePath).pipe(res);
-        } else {
-            // Valid range requested
-            const start = ranges[0].start;
-            const end = ranges[0].end;
-
-            const chunkSize = (end - start) + 1;
-            const stream = fs.createReadStream(filePath, { start, end });
-            res.writeHead(206, {
-                'Content-Type': 'video/mp4',
-                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                'Content-Length': chunkSize
-            });
-
-            stream.pipe(res);
-        }
-    } else {
-        res.end('Page non trouvée');
-    }
-});
-const ipAddress = '192.168.5.124'
-const port = 3003;
-server.listen(port, () => {
-    console.log(`Serveur en cours d'écoute http://${ipAddress}:${port}`);
 });
